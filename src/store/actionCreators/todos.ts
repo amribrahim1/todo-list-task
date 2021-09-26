01b47060dfda667ff0858166cc7b56b86bfde87b
Amr Eraky <amribrahim11@gmail.com>
2021-09-26 18:03:22 +0200

    Fix get Todos after Typescript

--- src/store/actionCreators/todos.ts
+++ src/store/actionCreators/todos.ts
@@ -21,13 +21,20 @@ export function handleGetTodos() {
     return (dispatch: Dispatch<TodoAction>) => {
         return getTodos()
         .then(docs => {
-            let todosArr: Todo[] = [];
+            let todosArr: any = [];
             docs.forEach((td) => {
-                todosArr.push({id: td.id, title: td.data().title, description: td.data().td.data()})
+                todosArr.push({id: td.id, ...td.data()})
+            })
+            const newArr : Todo[] = todosArr.map((tt:any) => {
+                return {
+                    id: tt.id,
+                    title: tt.title,
+                    description: tt.description
+                }
             })
             return dispatch({
                 type: ActionType.GET_TODOS,
-                todos: todosArr,
+                todos: newArr,
                 error: null
             })
         })
